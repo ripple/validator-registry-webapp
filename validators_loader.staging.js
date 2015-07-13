6eeb15c5cdcc8e1aeab2b09e09a5c36cdd88b573
Steven Zeiler <zeiler.steven@gmail.com>
2015-07-13 13:00:20 -0700

    [TASK] Update static validators_loader.#{NODE_ENV}.js

--- validators_loader.staging.js
+++ validators_loader.staging.js
@@ -1,30 +1,73 @@
+
 angular.module('validatorsApp').factory('ValidatorsLoader',['$http', function($http) {
 
   var VALIDATOR_REGISTRY_API = 'https://api-staging.validators.ripple.com';
 
+  function getValidations(callback) {
+      $http({
+        url: VALIDATOR_REGISTRY_API+"/validations",
+        method: "GET"
+      }).success(function(data, status, headers, config) {
+        callback(null, data.validations)
+      }).error(function(data, status, headers, config) {
+        callback(new Error(data))
+      })
+  }
+
+  function getValidators(callback) {
+      $http({
+        url: VALIDATOR_REGISTRY_API+"/validators",
+        method: "GET"
+      }).success(function(data, status, headers, config) {
+        callback(null, data.validators)
+      })
+      .error(function(data, status, headers, config) {
+        callback(new Error(data))
+      })
+  }
+
+  function reduceValidators(validators, validations) {
+    var reduced = []
+    validations.forEach(function(validation) {
+      var validator = _.find(validators, function(validator) {
+        return validator.validation_public_key === validation.validation_public_key
+      })
+      if (validator) {
+        validation.domain = validator.domain
+      }
+      reduced.push(validation)
+    })
+    return reduced
+  }
+
+  function sortByValidations(validations) {
+    return _.sortBy(validations, function(validation) {
+      return validation.validations_count * -1
+    })
+  }
+
   return (function() {
     return {
       scan: function(scope) {
-        $http({
-          url: VALIDATOR_REGISTRY_API+"/validators",
-          method: "GET"
-        }).success(function(data, status, headers, config) {
+        scope.loading = true
+
+        getValidations(function(error, validations) {
+          if (error) {
             scope.loading = false
-            scope.validators = data.validators;
-            data.metadata.forEach(function(item) {
-              scope.validators.forEach(function(validator) {
-                if (item.validation_public_key === validator.validation_public_key) {
-                  validator.validations_count = item.validations_count
-                }
-              })
+            scope.status = "Error connecting to API at " + VALIDATOR_REGISTRY_API;
+          } else {
+            getValidators(function(error, validators) {
+              scope.loading = false
+              if (error) {
+                scope.status = "Error connecting to API at " + VALIDATOR_REGISTRY_API;
+              } else {
+                scope.validators = sortByValidations(reduceValidators(validators, validations))
+              } 
             })
-        }).error(function(data, status, headers, config) {
-            scope.loading = true;
-            scope.status = "Error Connecting to Validator Registry API";
+          }
         })
       }
     }
   })()
 }])
   
-
