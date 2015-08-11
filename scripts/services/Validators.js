b4d87c593fb670880425ba0db401ef34a0afc4ee
Steven Zeiler <zeiler.steven@gmail.com>
2015-08-11 15:31:00 -0700

    [FIX] Download both /validators and /validations

--- scripts/services/Validators.js
+++ scripts/services/Validators.js
@@ -1,9 +1,23 @@
 
 angular.module('validatorsApp').factory('Validators',['$http', function($http) {
 
-  var validators
+  var combined
 
-  function fetch() {
+  function fetchValidations() {
+    return new Promise(function(resolve,reject) {
+      http
+        .get(window.config.VALIDATOR_REGISTRY_API+'/validations')
+        .end(function(error, response) {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(response.body.validations)
+          }
+        })
+    })
+  }
+
+  function fetchValidators() {
     return new Promise(function(resolve,reject) {
       http
         .get(window.config.VALIDATOR_REGISTRY_API+'/validators')
@@ -11,38 +25,64 @@ angular.module('validatorsApp').factory('Validators',['$http', function($http) {
           if (error) {
             reject(error)
           } else {
-            validators = response.body.validators
-            resolve(validators)
+            resolve(response.body.validators)
           }
         })
     })
   }
 
   function getValidators() {
-    if (validators) {
-      return new Promise(function(resolve, reject) {
-        resolve(validators)
-      })  
+    if (combined) {
+      return Promise.resolve(combined)
     } else {
-      return fetch()
+      return fetchValidations().then(function(validations) {
+
+        return fetchValidators().then(function(validators) {
+
+          combined = sortByValidations(reduceValidators(validators, validations))
+          return combined
+        })
+      })
     }
+  }
+
+  function sortByValidations(validations) {
+    return _.sortBy(validations, function(validation) {
+      return validation.validations_count * -1
+    })
+  }
+
+  function reduceValidators(validators, validations) {
+    var reduced = []
+    validations.forEach(function(validation) {
+      var validator = _.find(validators, function(validator) {
+        return validator.validation_public_key === validation.validation_public_key
+      })
+      if (validator) {
+        validation.domain = validator.domain
+      }
+      reduced.push(validation)
+    })
+    return reduced
   }
 
   function getDomain(validationPublicKey) {
-    var validator = _.find(validators, function(validator) {
-      return validator.validation_public_key === validationPublicKey
+    getValidators().then(function(validators) {
+      var validator = _.find(validators, function(validator) {
+        return validator.validation_public_key === validationPublicKey
+      })
+      if (validator) {
+        return validator.domain
+      } else {
+        return undefined
+      }
     })
-    if (validator) {
-      return validator.domain
-    } else {
-      return undefined
-    }
   }
 
   return {
-    fetch: fetch,
+    fetch: fetchValidators,
     getValidators: getValidators,
     getDomain: getDomain
   }
 }])
-  
+
